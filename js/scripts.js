5d2a8dedff4022337d979f5b6f92ff41ed887744
Ryan Jones <rjones.mis@gmail.com>
2019-08-16 13:45:27 -0700

    Added initial file structure, and forms html for basic questions

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+/*$(function(){
+  $("#blanks form").submit(function(event){
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      console.log(userInput);
+      $("." + blank).text(userInput).val();
+      return blank;
+    });
+
+    $("#story").show()
+
+    event.preventDefault()
+  })
+})
+*/
